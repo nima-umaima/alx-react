c59db54cbc2b25ae125b61c717f50312727c1dad
Nima <49175722+nima-umaima@users.noreply.github.com>
2023-08-16 15:47:41 +0300

    2-nested.js

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -3,5 +3,6 @@ import { fromJS } from 'immutable';
 export default function accessImmutableObject(object, array) {
   const mappedObj = fromJS(object);
 
+  
   return mappedObj.getIn(array, undefined);
 }
